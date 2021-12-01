23c9e923a8c5686e3cbd811574466df674f7aea6
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-12-01 08:41:58 +0700

    edit display show in admin page

--- store/admin.py
+++ store/admin.py
@@ -7,16 +7,26 @@ from .models.orders import Order
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ['name', 'price', 'category']
+    list_filter = ['category__name']
+    search_fields = ['name']
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
 
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['email', 'phone']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['product', 'customer', 'quantity']
+    list_filter = ['status', ]
+    search_fields = ['customer__first_name', 'customer__last_name']
+
+
 # Register your models here.
-admin.site.register(Products,AdminProduct)
+admin.site.register(Products, AdminProduct)
 admin.site.register(Category)
-admin.site.register(Customer)
-admin.site.register(Order)
-
-
-# username = Tanushree, email = tanushree7252@gmail.com, password = 1234
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
