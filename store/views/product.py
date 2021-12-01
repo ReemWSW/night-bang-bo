46bafb33321e46a57dfc168893916b2d159271a1
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-12-01 07:08:06 +0700

    category in producrt detial

--- store/views/product.py
+++ store/views/product.py
@@ -1,8 +1,14 @@
 from django.shortcuts import get_object_or_404, render
+from store.models.category import Category
 from store.models.product import Products
 
 
 def product_detail_view(request, id):
+    categories = Category.get_all_categories()
     product = Products.objects.filter(id=id)
+    context = {
+        "categories":categories,
+        "products":product
+    }
     print(product)
-    return render(request, "product.html", {"products":product})
+    return render(request, "product.html", context)
